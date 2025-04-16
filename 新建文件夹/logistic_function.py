6f6017411d0887e02f41b50c3df73f7c47986094
xiaoluosi <luyixun666@126.com>
2025-04-16 22:55:58 +0800

    2025/4/16

---
+++ 新建文件夹/logistic_function.py
@@ -0,0 +1,11 @@
+import torch.nn
+import torch.nn.functional as F
+
+class LogisticRegressionModel(torch.nn.Module):
+    def __init__(self):
+        super(LogisticRegressionModel, self).__init__()
+        self.linear = torch.nn.Linear(1, 1)
+
+    def forward(self, x):
+        y_pred = F.sigmoid(self.linear(x))
+        return y_pred
